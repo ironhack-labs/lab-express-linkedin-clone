73d26a498aca06e9488d59a91f548c0367a5c79e
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-28 10:08:48 +0200

    added signup page

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String },
+  email: { type: String/*, required: [true, 'Please enter the an email address']*/ },
+  password: { type: String/*, required: [true, 'Please enter a password']*/ },
+  summary: { type: String },
+  imageUrl: { type: String },
+  company: { type: String },
+  jobTitle: { type: String },
+});
+
+module.exports = mongoose.model('User', UserSchema);
