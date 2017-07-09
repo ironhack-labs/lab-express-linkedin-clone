d976ceec6e55df0d3332672ceb0be5a0afe90736
Marc Palatsí <mpalatsi@me.com>
2017-07-09 19:21:55 +0200

    basic get and post for signup achieved

---
+++ starter-code/routes/authController.js
@@ -0,0 +1,58 @@
+const express        = require("express");
+const authController = express.Router();
+
+// User model
+const User           = require("../models/user");
+
+// Bcrypt to encrypt passwords
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+
+authController.get("/signup", (req, res, next) => {
+  res.render("auth/signup"); // borrar comentario cuando la ruta este comprobada
+});
+
+authController.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const summary = req.body.summary;
+  const company = req.body.company;
+  const jobTittle = req.body.jobTittle;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", {
+        errorMessage: "The username already exists"
+      });
+      return;
+    }
+
+    var salt     = bcrypt.genSaltSync(bcryptSalt);
+    var hashPass = bcrypt.hashSync(password, salt);
+
+    var newUser = User({
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", {
+          errorMessage: "Something went wrong when signing up"
+        });
+      } else {
+        // User has been created...now what?
+      }
+    });
+  });
+});
+
+module.exports = authController;
