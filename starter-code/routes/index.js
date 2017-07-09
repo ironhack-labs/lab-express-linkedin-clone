233bc967086948ac57562afa1a61750d545c6479
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-07-09 13:40:25 +0200

    iteration 1

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    session: req.session.currentUser
+  });
 });
 
 module.exports = router;
