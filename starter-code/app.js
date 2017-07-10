e6cdf0adc18c29a00bb8e1f9f0e69c38009ec62e
Daniel Fernando Palma Lopez <palma@Daniels-MacBook-Pro.local>
2017-07-10 09:41:25 +0200

    1st iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,24 +4,40 @@ var favicon      = require('serve-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
+let mongoose = require('mongoose');
+let session = require('express-session');
+var MongoStore = require("connect-mongo")(session);
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+
+ mongoose.connect("mongodb://localhost/linkedin-clone");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 app.use('/', index);
 app.use('/users', users);
 
