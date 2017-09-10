e3a5351e56b79e244916bdd9db65150f642a90d0
Mercedes Egido <meegido@gmail.com>
2017-09-10 18:14:12 +0200

    login page

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -4,6 +4,14 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  res.redirect('/login')
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login')
+})
+
 module.exports = router;
+
+
+//If the user hasn't started a session, he should be redirected to /login page.
