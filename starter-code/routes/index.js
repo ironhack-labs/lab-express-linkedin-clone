51eb71e8db70d2d527bc47258bbb07e8caf48b09
Mercedes Egido <meegido@gmail.com>
2017-09-10 18:19:36 +0200

    sign up page

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -8,7 +8,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login')
+  res.render('login');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
 })
 
 module.exports = router;
