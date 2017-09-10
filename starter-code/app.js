c99ab01b5c01ee7e9d6eef55ea31361258d63f35
Álvar <alvar@MacBook-Air-de-Alvar.local>
2017-09-10 17:55:28 +0200

    1iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,25 @@
-var express      = require('express');
-var path         = require('path');
-var favicon      = require('serve-favicon');
-var logger       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
+const express      = require('express');
+const path         = require('path');
+const favicon      = require('serve-favicon');
+const logger       = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const mongoose       = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const { dbURL }      = require("./config/db");
+const session        = require("express-session");
+const MongoStore     = require("connect-mongo")(session);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const users    = require("./routes/users");
+const index    = require("./routes/index");
+const app = express();
 
-var app = express();
+mongoose.connect(dbURL);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +29,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.locals.title = "Linkedin";
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/routes/users.js
+++ starter-code/routes/users.js
@@ -1,9 +1,108 @@
-var express = require('express');
-var router = express.Router();
+
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const bcryptSalt = 12;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.get('/signup', (req, res) => {
+  if(req.session.currentUser){
+    res.redirect('/');
+  }else{
+  res.render('auth/signup', {
+    title: 'Signup'
+  });
+}
+});
+
+router.post('/signup', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "name": username }).then(user =>{
+    if(user){
+      res.render("auth/signup", {
+        errorMessage: "User already exists"
+      });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    new User({
+        name: username,
+        password: hashPass
+      })
+      .save()
+      .then(() => res.redirect('/'))
+      .catch(e => next(e));
+  });
+});
+
+router.get('/login', (req, res) => {
+  if(req.session.currentUser){
+    res.redirect('/');
+  }else{
+  res.render('auth/login', {
+    title: 'Login Here!'
+  });
+}
+});
+
+router.post("/login", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "name": username }, (err, user) => {
+      if (err || !user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  });
+});
+
+
+router.get("/profile",(req,res,next)=>{
+  res.render('auth/profile', {
+    title: 'Hola'
+  });
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    // cannot access session here
+    res.redirect("/login");
+  });
 });
 
 module.exports = router;
