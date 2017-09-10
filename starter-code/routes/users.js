037a2d7840e7f931d507712e7abc9188a69505d1
Luisca108 <luiscarlos3106@gmail.com>
2017-09-10 20:41:37 +0200

    database and routes created

--- starter-code/routes/users.js
+++ starter-code/routes/users.js
@@ -1,9 +1,67 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Register
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    company: req.body.company,
+    jobTitle: req.body.company
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({ success: false, msg: 'Failed to register user' });
+    } else {
+      res.json({ success: true, msg: 'User registered' });
+    }
+  });
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if (err) throw err;
+    if (!user) {
+      return res.json({ success: false, msg: 'User not found' });
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if (err) throw err;
+      if (isMatch) {
+        const token = jwt.sign(user, config.secret, {
+          expiresIn: 604800 // 1 week
+        });
+
+        res.json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email
+          }
+        });
+      } else {
+        return res.json({ success: false, msg: 'Wrong password' });
+      }
+    });
+  });
+});
+
+// Profile
+router.get('/profile', (req, res, next) => {
+  res.json({ user: req.user });
 });
 
 module.exports = router;
