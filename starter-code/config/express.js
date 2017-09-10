53b1a259ff455828a1222ba95e5221c28d32b484
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-10 13:57:25 +0200

    create ok

---
+++ starter-code/config/express.js
@@ -0,0 +1,20 @@
+const express      = require('express');
+const logger       = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const layouts      = require('express-ejs-layouts');
+const path         = require('path');
+
+module.exports = (app) => {
+  // view engine setup
+  app.set('views', path.join(__dirname, '../views'));
+  app.set('view engine', 'ejs');
+  app.set('layout','layout');
+
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(layouts);
+};
--- starter-code/routes/users.js
+++ starter-code/routes/users.js
@@ -1,9 +1,56 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const User = require('../models/User');
+const bcryptSalt = 10;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  res.render('index');
 });
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+  const email = req.body.email;
+
+  if (username === "" || password === "" || name ==="" | email ==="") {
+    res.render("signup", {
+      errorMessage: "Fill all the fields to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }).then(user =>{
+    if(user){
+      res.render("signup", {
+        errorMessage: "User already exists"
+      });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+    new User({
+        username: username,
+        password: hashPass,
+        name: name,
+        email: email
+      })
+      .save()
+      .then(() => res.redirect('/'))
+      .catch(e => next(e));
+  });
+});
+
+
+
+
+
+
 
 module.exports = router;
