d9fa48d481af3ec49b8e4f8194238965ead382dd
Mercedes Egido <meegido@gmail.com>
2017-09-10 18:28:33 +0200

    fixed root of each one in js

--- starter-code/routes/logout.js
+++ starter-code/routes/logout.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('login');
+  res.render('logout');
 });
-
 
 module.exports = router;
--- starter-code/routes/signup.js
+++ starter-code/routes/signup.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/signup', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('signup');
 });
 
