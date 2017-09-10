2e19ec6e76ac2a4932d1214c77ac8e9c435ab4ea
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-10 15:31:18 +0200

    login y logout ok

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,6 +1,14 @@
-const app = require('express')();
 const mongoose = require('mongoose');
 const globals = require('./config/globals');
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const layouts = require('express-ejs-layouts');
+const session = require("express-session");
+const path = require('path');
+const MongoStore = require("connect-mongo")(session);
+const app = express();
 
 mongoose.connect(globals.dbUrl)
   .then( () => console.log("Connected to db!"));
@@ -8,7 +16,25 @@ mongoose.connect(globals.dbUrl)
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-require('./config/express')(app);
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.set('layout','layout');
+
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  }));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(layouts);
 
 // const index = require('./routes');
 // app.use('/', index);
--- starter-code/routes/users.js
+++ starter-code/routes/users.js
@@ -4,15 +4,17 @@ const router = express.Router();
 const User = require('../models/User');
 const bcryptSalt = 10;
 
-/* GET users listing. */
+//INDEX
 router.get('/', (req, res) => {
   res.render('index');
 });
 
+//GET SignUp
 router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
+//POST SignUp - Creates a user in db
 router.post('/signup', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -47,10 +49,49 @@ router.post('/signup', (req, res) => {
   });
 });
 
+//GET Login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
+//POST Login
+router.post("/login", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
 
+  if (username === "" || password === "") {
+    res.render("login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
 
+  User.findOne({ "username": username }, (err, user) => {
+      if (err || !user) {
+        res.render("login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        console.log(req.session.currentUser);
+        res.redirect("/");
+      } else {
+        res.render("login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  });
+});
 
-
+// LOGOUT
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    // cannot access session here
+    res.redirect("login");
+  });
+});
 
 module.exports = router;
