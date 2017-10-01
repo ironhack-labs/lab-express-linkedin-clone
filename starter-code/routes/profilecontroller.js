7d1625773d8712a5219d04fd9d510faa92ed9203
Valentin Perot <perot.valentin@gmail.com>
2017-10-01 20:21:05 +0200

    fix bug iteration 2

--- starter-code/routes/profilecontroller.js
+++ starter-code/routes/profilecontroller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const profileController = express.Router();
-
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
 const User = require('../models/user');
 
 profileController.get('/:id', (req, res, next) => {
@@ -24,6 +25,28 @@ profileController.get('/:id/edit', (req, res, next) => {
             })
         })
     }
+})
+
+profileController.post('/:id/edit', (req, res, next) => {
+    const profileId = req.params.id;
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(req.body.password, salt);
+
+    const update = {
+        username: req.body.username,
+        password: req.body.password,
+        name : req.body.name,
+        email: req.body.email,
+        summary: req.body.summary,
+        company: req.body.company,
+        jobTitle: req.body.jobTitle
+    }
+
+    User.findByIdAndUpdate(profileId, update, (err, profile) => {
+        if(err) { return next(err) }
+
+        return res.redirect(`/profile/${profileId}`)
+    })
 })
 
 module.exports = profileController;
