8287c1f30e3d9e7b85a4472c6cfa505cf9fe5751
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-12 20:50:17 +0100

    Fixed broken iteration 2.. Finally.

--- starter-code/app.js
+++ starter-code/app.js
@@ -52,11 +52,11 @@ app.use('/', authRoutes);
 app.use('/', profileRoutes);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
