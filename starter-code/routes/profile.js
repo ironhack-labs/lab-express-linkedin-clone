ae4d9af6a8222b672689eb10734958295f8ba27e
Riftland <fran.mscr@gmail.com>
2017-11-12 22:56:31 +0100

    A bit more of second it

--- starter-code/routes/profile.js
+++ starter-code/routes/profile.js
@@ -6,32 +6,24 @@ const User = require("../models/User");
 //For protect the path
 router.get('/', (req, res, next) => {
   if (!req.session.currentUser) {
-    res.render('/');
+    res.redirect('/');//Esto te lleva a buscar la ruta en app.js
   }else {
-    res.render('profile/show',
-      {username: req.session.currentUser.username,
-       id: req.session.currentUser._id});
-  }
+    let id = req.session.currentUser._id;
+    console.log(id);
+    User.findById(id)
+      .then((user) => {
+        res.render('profile/show', {//Esto pinta la vista de la ruta indicada, llevando consigo el objeto en el segundo parámetro
+          user : user
+        });
+      }).catch((err) => {
+        return next(err);
+      });
+    }
 });
 
-router.get('/:id', (req, res,next) => {
-  let id = req.params.id;
-  User.findById(id)
-    .then((user) => {
-      res.render('/profile/edit');
-    }).catch((err) => {
-      return next(err);
-    });
-});
-
-// router.post('/:id', (req, res,next) => {
-//   let id = req.params.id;
-//   User.findById(id)
-//     .then((user) => {
-//       res.render('profile/edit');
-//     }).catch((err) => {
-//       return next(err);
-//     });
+//Para otro momento la edición del perfil
+// router.get('/:id', (req, res, next) => {
+//
 // });
 
 module.exports = router;
