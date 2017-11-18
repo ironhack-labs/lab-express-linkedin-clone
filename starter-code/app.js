dc8d90ac8a4c4f7fec6dd6ddbb44d167f712a505
Clementina <mataclemy@gmail.com>
2017-11-18 18:49:38 +0100

    login, signup and log out

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,33 +1,60 @@
-var express      = require('express');
-var path         = require('path');
-var favicon      = require('serve-favicon');
-var logger       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
+const express      = require('express');
+const path         = require('path');
+const favicon      = require('serve-favicon');
+const logger       = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const mongoose = require('mongoose');
+const expressLayouts = require ('express-ejs-layouts');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const authController = require('./routes/authController')
 
-var app = express();
+mongoose.connect ('mongodb://localhost/linkedin-lab-development');
+
+const index = require('./routes/index');
+// const privateRoutes = require('./routes/private');
+// const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// app.set('layout','layouts/layout');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(expressLayouts);;
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+app.use(session({
+   secret: "basic-auth-secret",
+   cookie: { maxAge: 60*60*24 }, //cuando tiempo lo guarda tu ordenador
+   store: new MongoStore({
+     mongooseConnection: mongoose.connection,
+     ttl: 24 * 60 * 60 // 1 day tiempo que dura en la base de datos
+   })
+ }));
+
+
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
+app.use('/',authController);
+// app.use('/', privateRoutes);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
