4d48bcee9aa8523d859e335ccd33a3cc0899fe64
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-19 10:11:39 +0100

    Feat: Signup page crete

--- starter-code/routes/auth.routes.js
+++ starter-code/routes/auth.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-// const authController = require('../controllers/auth.controller');
+const authController = require('../controllers/auth.controller');
 
-// router.get('/signup', authController.signup);
+router.get('/', authController.signup);
 // router.post('/signup', authController.doSignup);
 
 // router.get('/login', authController.login);
