47e7a87d14024db7bf537de57c6dafe3c983e2a5
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-19 20:05:21 +0100

    Feat: Iteration 2

--- starter-code/controllers/profile.controller.js
+++ starter-code/controllers/profile.controller.js
@@ -2,38 +2,67 @@ const User = require('../models/user.model');
 const moment = require('moment');
 
 module.exports.edit = (req, res, next) => {
-  console.log("Email = " + req.params.email);
-  const email = req.params.email;
+  if (req.params.email != req.session.currentUser.email) {
+    res.redirect("/");
+  } else {
+    console.log("Email = " + req.params.email);
+    const email = req.params.email;
+    User.findOne({
+        email: req.params.email
+      })
+      .then(user => {
+        console.log(user);
+        if (!user) {
+          next();
+        } else {
+          res.render("profile/edit", user);
+        }
+      })
+      .catch(error => next(error));
+  }
+};
+module.exports.update = (req, res, next) => {
+  if (req.body.email != req.session.currentUser.email) {
+    res.redirect("/");
+  } else {
+    const user = new User({
+      _id: req.session.currentUser._id,
+      name: req.body.name,
+      email: req.body.email,
+      summary: req.body.summary,
+      imageUrl: req.body.imageUrl,
+      company: req.body.company,
+      jobTitle: req.body.jobTitle
+    });
+    User.findByIdAndUpdate(user._id, user, {
+        new: true
+      })
+      .then(currentUser => {
+        req.session.currentUser = user;
+        res.redirect(`/profile/${req.session.currentUser.email}/edit`);
+      })
+      .catch(error => next(error));
+  }
+};
+
+module.exports.show = (req, res, next) => {
+  console.log("ID = " + req.params.userId);
+  const _id = req.params.userId;
   User.findOne({
-      email: req.params.email
+      _id: _id
     })
     .then(user => {
       console.log(user);
       if (!user) {
         next();
       } else {
-        res.render("profile/edit", user);
+        console.log(req.session.currentUser);
+        console.log(user);
+        res.render(`profile/show`, {
+          user: user,
+          sessionUser: req.session.currentUser
+        });
       }
-    })
-    .catch(error => next(error));
-};
-module.exports.update = (req, res, next) => {
-  const user = new User({
-    _id: req.session.currentUser._id,
-    name: req.body.name,
-    email: req.body.email,
-    summary: req.body.summary,
-    imageUrl: req.body.imageUrl,
-    company: req.body.company,
-    jobTitle: req.body.jobTitle
-  });
-  User.findByIdAndUpdate(user._id, user, {
-      new: true
-    })
-    .then(currentUser => {
-      // req.session.currentUser = currentUser;
-      res.send("ADIOS");
-      // res.redirect(`/profile/${username}`);
     })
     .catch(error => next(error));
 };
