8af892a5d60e6a57b9f541fa3199c432b2267179
Felix Heras <heras.felix@gmail.com>
2018-01-24 13:36:58 +0100

    feat:First iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,13 +4,20 @@ var favicon      = require('serve-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require("connect-mongo")(session);
+//var index = require('./routes/index');
+//var users = require('./routes/users');
+require('./configs/db.config');
+var auth = require('./routes/auth.routes');
 var app = express();
 
+
 // view engine setup
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,10 +27,25 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+   secret: 'Super Secret',
+   resave: false,
+   saveUninitialized: true,
+   cookie: {
+     secure: false,
+     httpOnly: true,
+     maxAge: 60 * 60 * 24 * 1000
+   },
+   store: new MongoStore({
+     mongooseConnection: mongoose.connection,
+     ttl: 24 * 60 * 60
+   })
+ }))
 
-app.use('/', index);
-app.use('/users', users);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', auth);
+//app.use('/', index);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
