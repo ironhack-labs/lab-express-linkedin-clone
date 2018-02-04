64bf013a3743333bf706dfa2c4982479489f9425
Ana Recuero <anarecsa@gmail.com>
2018-02-04 15:43:08 +0100

    view profile without session fix

--- starter-code/routes/profiles.js
+++ starter-code/routes/profiles.js
@@ -2,21 +2,20 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+// //GET EDIT
+// router.get("/profile/:id/edit",(req,res,next) => {
+//   const currentUser = req.session.currentUser;
+//   res.render("profiles/edit",{currentUser});
+// });
+
 // GET PROFILE 
 router.get("/profile/:id", (req,res,next) => {
   const userId = req.params.id;
-  let currentUser = req.session.currentUser;
-  
+  const currentUser = req.session.currentUser;
+
   User.findById(userId).exec().then( user => {
     console.log({currentUser})
-    res.render('profiles/show', {user: user, currentUser : currentUser});
-    
-    // if (currentUser){
-    //   res.render('profiles/show', {user: user}, {currentUser});
-    //   return;
-    // } else{
-    //   res.render('profiles/show', {user: user});
-    // }
+    res.render("profiles/show", {user: user, currentUser : currentUser});
   }).catch(e => next(e))
 });
 
