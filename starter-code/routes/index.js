67d4448ac6b99bfcba75bb683f1cfebc2cc6ee0c
Enrique Hernández <l6cenrique@gmail.com>
2018-02-05 00:42:52 +0100

    Intento de edit pero me marcho a dormir ya

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -4,17 +4,31 @@ const User = require("../models/User");
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'LinkedIn' });
 });
 
-router.get('/logout',(req,res) => {
+router.get('/logout', (req, res) => {
   req.session.currentUser = null;
   res.redirect('/');
 })
 
-router.get('/update/:id/edit', (req, res) => {
+
+
+router.get('/update/:id', (req, res) => {
   const userId = req.params.id;
+  console.log(req.params)
+  User.findById(userId, (err, user) => {
+    if (err) { return next(err); }
+    res.render('update', { user: user, });
+    console.log({ user });
+
+  })
+});
+
+router.get('/update/:id', (req, res) => {
+  const userId = req.params.id;
+  console.log(req.params.id)
   User.findById(userId, (err, user) => {
       if (err) { return next(err); }
       res.render('update', { user: user });
@@ -23,7 +37,7 @@ router.get('/update/:id/edit', (req, res) => {
 
 
 /* CRUD -> UPDATE DATABASE */
-router.post('/update/:id/edit', (req, res) => {
+router.post('/update/:id', (req, res) => {
   const userId = req.params.id;
   const {username, imageUrl} = req.body;
   const updates =  {username, imageUrl};
