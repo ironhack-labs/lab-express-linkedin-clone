c290bb8fb46b6c1e4763969b7d3e7b90a85f0fb5
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-05 15:40:56 +0100

    first commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,46 +1,82 @@
-var express      = require('express');
-var path         = require('path');
-var favicon      = require('serve-favicon');
-var logger       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 
-var app = express();
+const app = express();
+
+// -- database
+
+mongoose.connect('mongodb://localhost/linkedin-users', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// middleware
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// session
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use(function (req, res, next) { /* ===========> esto hace que podamos llamar user fields en un layout */
+  app.locals.user = req.session.currentUser;
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Routes
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use('/', index);
+app.use('/users', users);
+app.use('/auth', auth);
+
+// -- 404 and error handler
+
+// -- error handlers
+
+app.use(function (req, res, next) {
+  res.status(404);
+  const data = {
+    title: '404 Not Found'
+  };
+  res.render('not-found', data);
+});
+
+app.use(function (err, req, res, next) {
+  console.error('ERROR', req.method, req.path, err);
+  if (!res.headersSent) {
+    const data = {
+      title: '500 Ouch'
+    };
+    res.status(500);
+    res.render('error', data);
+  }
 });
 
 module.exports = app;
