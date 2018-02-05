7362a4eff80b87f9cd9695c2c209ec3b3241e93e
Martin <mar.gardemann@gmail.com>
2018-02-05 14:00:53 +0100

    iteration1

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Main homepage
+router.get('/', (req, res) => {
+  if (!req.session.currentUser) {
+    res.redirect('/login');
+  } else {
+    res.render('home', {user: req.session.currentUser});
+  }
 });
 
 module.exports = router;
--- starter-code/routes/users.js
+++ starter-code/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Main homepage
+router.get('/', (req, res) => {
+  if (!req.session.currentUser) {
+    res.redirect('/login');
+  } else {
+    res.render('home', {user: req.session.currentUser});
+  }
 });
 
 module.exports = router;
