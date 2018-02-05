eb32a16399ff2f2cdc74922c8148398fe4aef418
Rodrigo Daoud <rodaoud93@gmail.com>
2018-02-05 15:23:10 +0100

    done

--- starter-code/routes/profiles.js
+++ starter-code/routes/profiles.js
@@ -4,7 +4,16 @@ const User       = require("../models/user");
 const profileRoutes = express.Router();
 
 profileRoutes.get("/show", (req, res, next) => {
-    res.render("profiles/show");
+
+    const data = {
+        theUser: req.session.currentUser
+    }
+
+    res.render("profiles/show", data);
+});
+
+profileRoutes.get("/:id", (req, res, next) => {
+    const id = req.params.id;
 });
 
 module.exports = profileRoutes;
