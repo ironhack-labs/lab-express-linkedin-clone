6eca68a9b5fb276925ad9ca9accc0073969dbaf1
Jana Stinn <jana-stinn@gmx.de>
2018-02-05 11:46:58 +0100

    login signup

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -5,13 +5,43 @@ const router = express.Router();
 
 /* render the login form. */
 router.get('/login', (req, res, next) => {
-  // if (req.session.currentUser) {
-  //   return res.redirect('/');
-  // }
   const data = {
     title: 'Login'
   };
   res.render('authentication/login', data);
+});
+
+router.get('/signup', (req, res, next) => {
+  const data = {
+    title: 'Signup'
+  };
+  res.render('authentication/signup', data);
+});
+
+router.get('/logout', (req, res, next) => {
+  res.redirect('authentication/login');
+});
+
+router.post('/login', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const data = {
+    title: 'Welcome'
+  };
+  res.render('/', data);
+});
+
+router.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const data = {
+    title: 'Welcome'
+  };
+  res.render('/', data);
 });
 
 module.exports = router;
