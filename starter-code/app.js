07433226ba91063c0a35f1cd366865f8983e76d9
Santi Ramos <santi92.network@gmail.com>
2018-02-05 15:55:57 +0100

    conect to DB

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,46 +1,67 @@
-var express      = require('express');
-var path         = require('path');
-var favicon      = require('serve-favicon');
-var logger       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
+'use strict'
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express      = require('express');
+const path         = require('path');
+const favicon      = require('serve-favicon');
+const logger       = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const mongoose     = require('mongoose');
 
-var app = express();
+// npm install --save express-session connect-mongo
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
-// view engine setup
+const app = express();
+
+const authRoutes = require('./routes/auth-routes');
+// Removed because of instructions
+// const index = require('./routes/index');
+// const users = require('./routes/users');
+
+// Mongoose configuration
+mongoose.connect("mongodb://localhost/linkedin");
+
+// -- view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// -- middlewares
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// -- session
+// Removed because of instructions
+// app.use('/', index);
+// app.use('/users', users);
+app.use('/auth', authRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+
+// -- 404 and error handler
+
+// NOTE: requires a views/not-found.ejs template
+app.use(function (req, res, next) {
+  res.status(404);
+  res.render('not-found');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// NOTE: requires a views/error.ejs template
+app.use(function (err, req, res, next) {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
 });
+
 
 module.exports = app;
